a67ab0258c30e0762d3083bc3628703a10b88a1f
gleb_z <118896591+everneverevernever@users.noreply.github.com>
2023-05-05 09:28:45 +0300

    Add files via upload

--- RoomLibrary/Room.cs
+++ RoomLibrary/Room.cs
@@ -46,6 +46,50 @@ namespace RoomLibrary
         {
             return RoomArea() / np;
         }
+        public string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + " кв.м";
+        }
+    }
+    /// <summary>
+    /// Класс "Жилая комната"
+    /// </summary>
+    public class LivingRoom : Room
+    {
+        int numWin; // число окон
+        public int NumWin
+        { get { return numWin; } set { numWin = value; } }
+        /// <summary>
+        /// Метод возвращает информацию о комнате
+        /// </summary>
+        /// <returns>Возвращается строка с информацией</returns>
+        public string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + " кв.м, с " + numWin + " окнами";
+        }
+    }
+    public class Office : Room
+    {
+        int numSockets; // число розеток
+        public int NumSockets
+    { get { return numSockets; } set { numSockets = value; } }
+    /// <summary>
+    /// Возвращает максимально возможное число рабочих мест
+    /// </summary>
+    /// <returns>Возвращается число мест</returns>
+    public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+    /// <summary>
+    /// Метод возвращает информацию об офисе
+    /// </summary>
+    /// <returns>Возвращается строка с информацией</returns>
+    public string Info()
+        {
+            return "Офис на " + NumWorkplaces() + " рабочих мест"; 
+        }
     }
 }
 
--- Rooms/MainWindow.xaml.cs
+++ Rooms/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoomLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,67 @@ namespace Rooms
     /// </summary>
     public partial class MainWindow : Window
     {
+        Room room = new Room();
+        Office office = new Office();
+        LivingRoom livingRoom = new LivingRoom();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        List<Room> lstRooms = new List<Room>(); // Создание списка комнат 
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            lstRooms.Add(room);
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            lstRooms.Add(livingRoom);
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+            lstRooms.Add(office);
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            foreach (Room r in lstRooms)
+                ListRooms.Content += r.Info() + "\n";
+        }
+
+        private void BAddRoom_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BAddLivingRoom_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BAddOffice_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BGetList_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
